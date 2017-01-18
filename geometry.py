ddad858e94a0ee5aa987a427ff53e53ae7d8e8e7
Anna Matras <ania.matras@gmail.com>
2017-01-18 18:37:52 +0100

    Version with 26 test passed

--- geometry.py
+++ geometry.py
@@ -1,3 +1,5 @@
+import math
+
 class Shape:
     """
     This is a abstract class representing geometrical shape.
@@ -11,6 +13,7 @@ class Shape:
             ValueError: If any of the parameters is below 0.
         """
         pass
+
 
     def get_area(self):
         """
@@ -61,28 +64,157 @@ class Shape:
 
 
 class Circle(Shape):
-    pass
+    def __init__(self, r):
+        Shape.__init__(self)
+        self.r = r
+        if r < 0:
+            raise ValueError("Values of parameters can't be negative.")
 
+    def get_area(self):
+        area = math.pi*self.r**2
+        return area
+
+    def get_perimeter(self):
+        perimeter = 2*math.pi*self.r
+        return perimeter
+
+    def __str__(self):
+        return "Circle, r " % str(self.r)
 
 class Triangle(Shape):
-    pass
+    def __init__(self, a, b, c):
+        Shape.__init__(self)
+        self.a = a
+        self.b = b
+        self.c = c
+        if a < 0 or b < 0 or c < 0:
+            raise ValueError("Values of parameters can't be negative.")
+
+    def get_area(self):
+        s = (self.a + self.b + self.c) / 2
+        area = math.sqrt(s*(s-self.a)*(s-self.b)*(s-self.c))
+        return area
+
+    def get_perimeter(self):
+        perimeter = self.a+self.b+self.c
+        return perimeter
+
+    def __str__(self):
+        return " Triangle, a = %s, b = %s, c= %s" % str(self.a, self.b, self.c)
 
 
 class EquilateralTriangle(Triangle):
-    pass
+    def __init__(self, a):
+        Triangle.__init__(self, a, b=a, c=a)
+        if a < 0:
+            raise ValueError("Values of parameters can't be negative.")
+
+    def __str__(self):
+        return "Equilateral Triangle, a = " + str(self.a)
+
+    def get_area(self):
+        s = (self.a+self.b+self.c)/2
+        area = math.sqrt(s*(s - self.a)*(s - self.b)*(s - self.c))
+        return area
+
+    def get_perimeter(self):
+        perimeter = self.a+self.b+self.c
+        return perimeter
 
 
 class Rectangle(Shape):
-    pass
+    def __init__(self, a, b):
+        Shape.__init__(self)
+        self.a = a
+        self.b = b
+        if a < 0 or b < 0:
+            raise ValueError("Values of parameters can't be negative.")
+
+    def get_area(self):
+        area = self.a*self.b
+        return area
+
+    def get_perimeter(self):
+        perimeter = 2*self.a + 2*self.b
+        return perimeter
 
 
 class Square(Rectangle):
-    pass
+    def __init__(self, a):
+        Rectangle.__init__(self, a, b=a)
+        if a < 0:
+            raise ValueError("Values of parameters can't be negative.")
+
+    def get_area(self):
+        area = self.a**2
+        return area
+
+    def get_perimeter(self):
+        perimeter = self.a * 4
+        return perimeter
 
 
 class RegularPentagon(Shape):
-    pass
+    def __init__(self, a):
+        Shape.__init__(self)
+        self.a = a
+        if a < 0:
+            raise ValueError("Values of parameters can't be negative.")
+
+    def get_area(self):
+        area = self.a**2 * math.sqrt(5*(5+2*math.sqrt(5)))/4
+        return area
+
+    def get_perimeter(self):
+        perimeter = 5 * self.a
+        return perimeter
 
 
 class ShapeList:
-    pass
+    def __init__(self):
+        pass
+
+    def add_shape(self, shape):
+        if not isinstance(shape, Shape):
+            raise TypeError
+        shapes = []
+        shapes.append(shape)
+        return shapes
+
+    def get_shapes_table(self, table, title_list):
+        # len_for_col = []
+        # for title_iterator in range(len(title_list)):
+        #     len_col = len(title_list[title_iterator])
+        #     for row in table:
+        #         if len(row[title_iterator]) > len_col:
+        #             len_col = len(row[title_iterator])
+        #
+        #     len_for_col.append(len_col)
+        #
+        # how_wide = 0
+        # for name in title_list:
+        #     x = (len_for_col[title_list.index(name)])
+        #     how_wide += (len(("|{: <" + str(x + 2) + "}").format(name)))
+        # print('-' * how_wide)
+        #
+        # for name in title_list:
+        #     print("|", end="")
+        #     x = (len_for_col[title_list.index(name)])
+        #     print(("{: <" + str(x + 2) + "}").format(name), end="")
+        # print("|")
+        # print('-' * how_wide)
+        #
+        # for row in table:
+        #     print("|", end="")
+        #     for element in row:
+        #         x = (len_for_col[row.index(element)])
+        #         print(("{: <" + str(x + 2) + "}|").format(element), end="")
+        #     print()
+        # print('-' * how_wide)
+        pass
+
+    def get_largest_shape_by_perimeter(self):
+        pass
+
+    def get_largest_shape_by_area(self):
+        pass
--- main.py
+++ main.py
@@ -6,23 +6,26 @@ def main():
 
     shapes = ShapeList()  # object containing all shapes added by the user
     while True:
-        # TODO: implement user interaction here. You can change the code below
+        print("""Learn Geometry.
+            What do you want to do?
+            (1) Add new shape
+            (2) Show all shapes
+            (3) Show shape with the largest perimeter
+            (4) Show shape with the largest area
+            (5) Show formulas
+            (0) Exit program""")
         option = input("Select an option: ")
         if option == "1":
-            # Add new shape
-            pass
+            shapes.add_shape(input("Add the shape:"))
         elif option == "2":
-            # Show all shapes
-            pass
+            ShapeList.get_shapes_table()
         elif option == "3":
-            # Show shape with the largest perimeter
-            pass
+            ShapeList.get_largest_shape_by_perimeter()
         elif option == "4":
-            # Show shape with the largest area
-            pass
+            ShapeList.get_largest_shape_by_area()
         elif option == "5":
-            # Show formulas
-            pass
+            Shape.get_perimeter_formula()
+            Shape.get_area_formula()
         elif option == "0":
             sys.exit()
 
